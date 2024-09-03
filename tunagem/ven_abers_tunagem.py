3d9df58d3cabea3daff6981efcde9f12641a657c
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2024-09-03 09:41:37 -0300

    Passando try para os caminho do diretorio no actions

--- tunagem/ven_abers_tunagem.py
+++ tunagem/ven_abers_tunagem.py
@@ -16,8 +16,13 @@ root_dir = os.path.abspath(os.path.join(os.getcwd(), '..'))
 # Constrói o caminho absoluto para o arquivo 'dados_tratados.pkl' dentro da pasta 'dados'
 diretorio = os.path.join(root_dir, 'dados', 'dados_tratados.pkl')
 
-with open(diretorio, 'rb') as f:
-    dados_tratados = pickle.load(f)
+try:
+    with open(diretorio, 'rb') as f:
+        dados_tratados = pickle.load(f)
+except FileNotFoundError:
+    print('Estamos dentro do GitHub Actions')
+    with open('/home/runner/work/Prediction-poisonous-mushrooms/dados/dados_tratados.pkl', 'rb') as f:
+        dados_tratados = pickle.load(f)    
 
 x_train = dados_tratados['x_train']
 y_train = dados_tratados['y_train']
@@ -30,13 +35,23 @@ diretorio = diretorio = os.path.join(root_dir, 'modelos')
 
 ## fazendo dowload dos modelos
 
-with open(diretorio + '/modelo_random_forest.pkl', 'rb') as f:
-    modelo = pickle.load(f)
+try:
+    with open(diretorio + '/modelo_random_forest.pkl', 'rb') as f:
+        modelo = pickle.load(f)
+except FileNotFoundError:
+    print('Estamos dentro do GitHub Actions')
+    with open('/home/runner/work/Prediction-poisonous-mushrooms/modelos/modelo_random_forest.pkl', 'rb') as f:
+        dados_tratados = pickle.load(f) 
         
-
 va_grad = VennAbersCalibrator(estimator=modelo.best_estimator_, inductive=False, n_splits=10)
 va_grad.fit(x_train, y_train)
-with open(diretorio + f'/gradiente_boost_base_ven.pkl', 'wb') as f:
-      pickle.dump(va_grad, f)
+
+try:
+    with open(diretorio + f'/gradiente_boost_base_ven.pkl', 'wb') as f:
+        pickle.dump(va_grad, f)
+except FileNotFoundError:
+    print('Estamos dentro do GitHub Actions')
+    with open('/home/runner/work/Prediction-poisonous-mushrooms/modelos/gradiente_boost_base_ven.pkl', 'wb') as f:
+        pickle.dump(va_grad, f)
       
-      
+print('Modelo Calibrado com sucesso!')
